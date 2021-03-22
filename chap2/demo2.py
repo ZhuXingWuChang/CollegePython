ac77e2518ab9f2414d4eff536214c1f50613652e
诸行无常 <2061007923@qq.com>
2021-03-22 22:25:02 +0800

    Python语法

--- chap2/demo2.py
+++ chap2/demo2.py
@@ -1,5 +1,7 @@
 name = '玛丽亚'
 print(name)
-print('标识', id(name))
+# id函数返回变量的地址
+print('地址', id(name))
+# type函数返回变量的类型
 print('类型', type(name))
 print('值', name)
--- chap2/demo5.py
+++ chap2/demo5.py
@@ -2,8 +2,8 @@ a = 3.14159
 print(a, type(a))
 n1 = 1.1
 n2 = 2.2
-print(n1 + n2)
+print(n1 + n2)  # 二进制数无法精确表示所有十进制数，存在误差
 
 from decimal import Decimal
 
-print(Decimal('1.1') + Decimal('2.2'))
+print(Decimal('1.1') + Decimal('2.2'))  # 可以Decimal函数解决精度问题
