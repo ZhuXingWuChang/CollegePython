986be4225eb26ea0b626dea57a60cf6f2e48fba3
ZhuXingWuChang <2061007923@qq.com>
2021-03-29 16:53:04 +0800

    learn to spyder

--- spider/requests模块基础/requests国家药监总局.py
+++ spider/requests模块基础/requests国家药监总局.py
@@ -11,15 +11,29 @@ url: http://scxk.nmpa.gov.cn:81/xk/
     - 先试着将一家企业的详情页的详情数据爬取出来,然后再取爬取多家企业的数据
 """
 import requests
+import json
 
 if __name__ == '__main__':
-    url = 'http://scxk.nmpa.gov.cn:81/xk/'
+    page_url = 'http://scxk.nmpa.gov.cn:81/xk/itownet/portalAction.do?method=getXkzsList'
     headers = {
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; '
-                      'x64) AppleWebKit/537.36 (KHTML, like '
-                      'Gecko) Chrome/89.0.4389.90 Safari/537'
-                      '.36 Edg/89.0.774.63'
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36 Edg/89.0.774.63'
     }
-    page_text = requests.get(url=url, headers=headers).text
-    with open('./化妆品.html', 'w', encoding='utf=8') as fp:
-        fp.write(page_text)
+    for page_num in range(2):
+        page_params = {
+            'on': 'true',
+            'page': str(page_num),
+            'pageSize': '15',
+            'productName': '',
+            'conditionType': '1',
+            'applyname': '',
+        }
+        page_response_json = requests.post(url=page_url, params=page_params, headers=headers).json()
+        for i in page_response_json['list']:
+            item_id = i['ID']
+            item_url = 'http://scxk.nmpa.gov.cn:81/xk/itownet/portalAction.do?method=getXkzsById'
+            item_params = {
+                'id': item_id
+            }
+            item_response_json = requests.post(url=item_url, params=item_params, headers=headers).json()
+            print("*企业负责人:", item_response_json['businessPerson'], " *企业名称:", item_response_json['epsName'],
+                  " *许可项目:", item_response_json['certStr'])
